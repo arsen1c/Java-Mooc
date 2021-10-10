55117a14f3cfb86425690d93ca2588602764c856
Arsenic <vector2912@gmail.com>
2021-10-10 21:38:21 +0530

    finished reading files

--- Part4/3_Files/Main.java
+++ Part4/3_Files/Main.java
@@ -1,4 +1,5 @@
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main {
@@ -21,13 +22,21 @@ class Main {
       - Reading a file may result in an error, and it's for this readon that the
         process requires seperate blocks - one for the "try" and another to "catch"
         potential errors.
+
+      - To check for empty lines, do: "line.isEmpty()"
     */
-    readingFile();
+
+    // Run methods
+
+    // readingFile();
+    // linesInFile();
+    // checkName();
+    numbersFromAFile();
   }
 
   public static void readingFile() {
     // create a scanner for reading the file
-    try (Scanner scanner = new Scanner(Paths.get("file.txt"))) {
+    try (Scanner scanner = new Scanner(Paths.get("./files/file.txt"))) {
       // we read the file until all the lines have been read
       while(scanner.hasNextLine()) {
         // we read one line
@@ -38,5 +47,75 @@ class Main {
     } catch (Exception e) {
       System.out.println("Error: " + e);
     }
+  }
+
+  public static void linesInFile() {
+    ArrayList<String> lines = new ArrayList<String>();
+
+    // Scanner for reading the file
+    try(Scanner scanner = new Scanner(Paths.get("./files/file.txt"))) {
+      while(scanner.hasNextLine()) {
+        lines.add(scanner.nextLine());
+      }
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+    
+    System.out.println("Total lines: " + lines.size());
+  }
+
+  // check if a name exists in a file 
+  public static void checkName() {
+    ArrayList<String> list = new ArrayList<String>();
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Search for: ");
+    String nameToCheck = reader.nextLine();
+    reader.close();
+
+    try(Scanner scanner = new Scanner(Paths.get("./files/names.txt"))) {
+      while(scanner.hasNextLine()) {
+        list.add(scanner.nextLine());
+      }
+    } catch (Exception e) {
+      System.out.println("Error: " + e);
+    }
+
+    if (list.contains(nameToCheck)) {
+      System.out.println("The name is on the list");
+    } else {
+      System.out.println("Not found");
+    }
+  }
+
+  /*
+    [+] Numbers from a file
+    - Write a program that prompts the user for a filename, as well as the upper
+      and lower bounds for the accepted range of numbers.
+
+    Sample output:
+
+      File?         numbers-1.txt
+      Lower bound?  15
+      Upper bound?  20
+      Numbers:      2
+  */
+  public static void numbersFromAFile() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number to search: ");
+    int number = Integer.valueOf(scanner.nextLine());
+
+    ArrayList<Integer> numberList = new ArrayList<Integer>();
+
+    try(Scanner reader = new Scanner(Paths.get("./files/numbers.txt"))) {
+      while(reader.hasNextLine()) {
+        numberList.add(Integer.valueOf(reader.nextLine()));
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    System.out.println(numberList.contains(number));
+
+    scanner.close();
   }
 }
